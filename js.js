4221b0fd28834d02bb1e327669b91828bd54b573
Ádám <nagy.adam.tamas@gmail.com>
2019-04-23 16:08:17 +0200

    ui almost done TODO: placestogo

--- js.js
+++ js.js
@@ -31,6 +31,25 @@ let characters = [
         fight: [10, 20],
         abilities: null,
     },
+    {
+        name: "test char 2",
+        description: "description for character number one",
+        hp:[100,100],
+        fight: [10, 20],
+        abilities: null,
+    },
+    {
+        name: "test char 3",
+        description: "description for character number one",
+        hp:[100,100],
+        abilities: null,
+    },
+    {
+        name: "test char 4",
+        description: "description for character number one",
+        hp:[100,100],
+        abilities: null,
+    },
 ];
 
 let abilities = [
@@ -71,29 +90,45 @@ let maps = [
         mapMusic: null,
         mapArriveText: "Test map arrive text. Lets make this long to see how it behaves in a multi-line setting. Lorem ipsum dolor sit amet.",
         mapArriveEvent: function(){
-			writeText(`arrived at ${this.mapName}`);
+			console.log(`arrived at ${this.mapName}`);
 		},
         mapMenu: {
             personsAtPlace: [
                 {
-                    personName: "Person1",
+                    personName: "Person 1",
                     personDescription: "Person1 Desc. Lorem ipsum dolor sit amet.",
-                    personTalk: ["BESZÉLEK VELE", "", ""],
-                    personRomance: ["KIKEZDEK VELE", "", ""],
-                }
+                    personTalk: ["talk test 1", "talk test 2", function(){writeText("TALKFUNCTION!")}],
+                    personRomance: ["romance test 1", function(){writeText("ROMANCEFUNCTION!")}, "romance test 3 final"],
+                    personTalkedTo: 0,
+                    personRomanced: 0
+                },
+                {
+                    personName: "Person2",
+                    personDescription: "Person2 Desc.",
+                    personTalk: ["", ""],
+                    personRomance: ["", ""],
+                    personTalkedTo: 0,
+                    personRomanced: 0
+                },
+                {
+                    personName: "Person 3",
+                    personDescription: "Person2 Desc. Lorem ipsum dolor sit amet.",
+                    personTalk: ["talk test 1", "talk test 2", "talk test 3 final"],
+                    personRomance: ["romance test 1", "romance test 2"],
+                    personTalkedTo: 0,
+                    personRomanced: 0
+                },
             ],
-            searched: false,
-            searchText: ["KERESEK", ""],
-            searchFind: function(){
-                console.log("searchTempText")
-            },
             pointsOfInterest: [
                 {
+                    poiName: "Keresek",
+                    poiEvent: ["", function(){writeText("PoI text for stuff", "poi text title")}, ""],
+                    poiDone: 0
+                },
+                {
                     poiName: "Point Of Interest Number One",
-                    poiText: "Test Text for PoI number ONE, lets make this long to see how it behaves in a multi-line setting. Lorem ipsum dolor sit amet.",
-                    poiEvent: function(){
-                        console.log("poi event");
-                    },
+                    poiEvent: ["", "", function(){writeText("PoI text for stuff", "poi text title")}],
+                    poiDone: 0
                 }
             ],
             placesToGo: [
@@ -104,13 +139,6 @@ let maps = [
     },
 ];
 
-//CURRENT STATE OF CHARACTERS, MAPS AND STUFF
-
-var CURRENT_STATE = {
-    currentMap: maps[0],
-    party: [characters[0]],
-    inventory: [items[0], items[1]],
-}
 
 // --------------------------------------------------------------------------
 
@@ -125,27 +153,40 @@ const reclickable = () => {
 	divRightMenu.style.pointerEvents = "auto";
 }
 
-// TEXT EVENTS
-const writeText = (text) => {
+// WRITE TEXT, WITH OPTIONAL TITLE
+const writeText = (text, title = "") => {
     let currentText = document.createElement("div");
     currentText.classList += "eventText textToOrder";
+
+    if(title != ""){
+        currentText.appendChild(document.createTextNode(title.toUpperCase()));
+        currentText.appendChild(document.createElement("br"));
+        currentText.appendChild(document.createElement("br"));
+    }
+
     currentText.appendChild(document.createTextNode(text));
     divLeftText.appendChild(currentText);
 }
 
 
-//LOAD PARTY
-const loadParty = () => {CURRENT_STATE.party.map((member) => {
+// LOAD PARTY
+const loadParty = () => {
+    menuParty.innerHTML = "";
+    CURRENT_STATE.party.map((member) => {
+    
     let currentMember = document.createElement("div");
     currentMember.classList += "party_member menu_subsubdivs";
 
     let currentSpan = document.createElement("span");
-    currentSpan.appendChild(document.createTextNode(member.name));
+    currentSpan.appendChild(document.createTextNode(member.name.toUpperCase()));
     currentMember.appendChild(currentSpan);
+
     currentMember.appendChild(document.createElement("br"));
+
     currentSpan = document.createElement("em");
     currentSpan.appendChild(document.createTextNode(member.description));
     currentMember.appendChild(currentSpan);
+
     let currentDetails = document.createElement("div");
     currentDetails.classList += "details";
 
@@ -153,30 +194,59 @@ const loadParty = () => {CURRENT_STATE.party.map((member) => {
     currentSpan = document.createElement("span");
     currentSpan.appendChild(document.createTextNode(`HP: ${member.hp[0]}/${member.hp[1]}`));
     currentDetails.appendChild(currentSpan);
-    currentSpan = document.createElement("span");
-    currentSpan.appendChild(document.createTextNode(`Mana: ${member.mana[0]}/${member.mana[1]}`));
-    currentDetails.appendChild(currentSpan);
-    currentSpan = document.createElement("span");
-    currentSpan.appendChild(document.createTextNode(`Harc: ${member.fight[0]}-${member.fight[1]}`));
-    currentDetails.appendChild(currentSpan);
+
+    if(member.hasOwnProperty("mana")){
+        currentSpan = document.createElement("span");
+        currentSpan.appendChild(document.createTextNode(`Mana: ${member.mana[0]}/${member.mana[1]}`));
+        currentDetails.appendChild(currentSpan);
+    }
+
+    if(member.hasOwnProperty("fight")){
+        currentSpan = document.createElement("span");
+        currentSpan.appendChild(document.createTextNode(`Harc: ${member.fight[0]}-${member.fight[1]}`));
+        currentDetails.appendChild(currentSpan);
+    }
 
     currentMember.appendChild(currentDetails);
 
     menuParty.appendChild(currentMember);
 })}
 
-//LOAD INVENTORY
-const loadInventory = () => {CURRENT_STATE.inventory.map((item) => {
+const addToParty = (name) => {
+    for(i = 0; i < characters.length; i++){
+        if(characters[i].name == name){
+            CURRENT_STATE.party.push(characters[i]);
+        }
+    }
+    loadParty();
+}
+
+const removeFromParty = (name) => {
+    for(i = 0; i < CURRENT_STATE.party.length; i++){
+        if(CURRENT_STATE.party[i].name == name){
+            CURRENT_STATE.party.splice(i, 1);
+        }
+    }
+    loadParty();
+}
+
+// LOAD INVENTORY
+const loadInventory = () => {
+    menuInventory.innerHTML = "";
+    CURRENT_STATE.inventory.map((item) => {
     let currentItem = document.createElement("div");
     currentItem.classList += "menu_subsubdivs item";
 
     let currentSpan = document.createElement("span");
-    currentSpan.appendChild(document.createTextNode(item.itemName));
+    currentSpan.appendChild(document.createTextNode(item.itemName.toUpperCase()));
     currentItem.appendChild(currentSpan);
+
     currentItem.appendChild(document.createElement("br"));
+
     currentSpan = document.createElement("em");
     currentSpan.appendChild(document.createTextNode(item.itemDescription));
     currentItem.appendChild(currentSpan);
+
     currentItem.appendChild(document.createElement("br"));
 
     if(item.hasOwnProperty("itemUseFunction")){
@@ -187,6 +257,24 @@ const loadInventory = () => {CURRENT_STATE.inventory.map((item) => {
     };
     menuInventory.appendChild(currentItem);
 })}
+
+const addToInventory = (name) => {
+    for(i = 0; i < items.length; i++){
+        if(items[i].itemName == name){
+            CURRENT_STATE.inventory.push(items[i]);
+        }
+    }
+    loadInventory();
+}
+
+const removeFromInventory = (name) => {
+    for(i = 0; i < CURRENT_STATE.inventory.length; i++){
+        if(CURRENT_STATE.inventory[i].itemName == name){
+            CURRENT_STATE.inventory.splice(i, 1);
+        }
+    }
+    loadInventory();
+}
 
 //LOAD MAP
 const loadMap = (currentMap) => {
@@ -215,18 +303,28 @@ const loadMap = (currentMap) => {
     currentStuff = document.createElement("p");
     currentStuff.appendChild(document.createTextNode(CURRENT_STATE.currentMap.mapArriveText));
     divLeftText.appendChild(currentStuff);
-    console.log(CURRENT_STATE.currentMap.personsAtPlace);
+    
+    // ARRIVE EVENT
+    if(CURRENT_STATE.currentMap.hasOwnProperty("mapArriveEvent")){
+        CURRENT_STATE.currentMap.mapArriveEvent();
+    }
     
     //PEOPLE AT MAP
     CURRENT_STATE.currentMap.mapMenu.personsAtPlace.map((person => {
         let currentPerson = document.createElement("div");
-        currentPerson.appendChild(document.createTextNode(person.personName));
+        currentPerson.classList += "person_at_place";
+
+        currentPerson.appendChild(document.createTextNode(person.personName.toUpperCase()));
         currentPerson.appendChild(document.createElement("br"));
+
         let currentStuff = document.createElement("em");
         currentStuff.appendChild(document.createTextNode(person.personDescription));
         currentPerson.appendChild(currentStuff);
+
         currentPerson.appendChild(document.createElement("br"));
+
         menuPeople.appendChild(currentPerson);
+
         let personButtonArea = document.createElement("div");
         personButtonArea.classList += "person_buttons";
         currentPerson.appendChild(personButtonArea);
@@ -234,14 +332,14 @@ const loadMap = (currentMap) => {
         if(person.hasOwnProperty("personTalk")){
             let currentButton = document.createElement("div");
             currentButton.classList += "clickable";
-            currentButton.appendChild(document.createTextNode(person.personTalk[0]));
+            currentButton.appendChild(document.createTextNode("BESZÉLEK VELE"));
             currentButton.id=`talk_${person.personName}`;
             personButtonArea.appendChild(currentButton);
         }
         if(person.hasOwnProperty("personRomance")){
             let currentButton = document.createElement("div");
             currentButton.classList += "clickable";
-            currentButton.appendChild(document.createTextNode(person.personRomance[0]))
+            currentButton.appendChild(document.createTextNode("KIKEZDEK VELE"))
             currentButton.id=`romance_${person.personName}`;
             personButtonArea.appendChild(currentButton);
         }
@@ -259,20 +357,61 @@ const loadMap = (currentMap) => {
     //PLACES TO GO
     CURRENT_STATE.currentMap.mapMenu.placesToGo.map((ptg => {
         let currentPTG = document.createElement("div");
-        currentPTG.appendChild(document.createTextNode(ptg.toString()));
+        currentPTG.appendChild(document.createTextNode(ptg.toString().toUpperCase()));
         currentPTG.classList += "clickable";
         currentPTG.id = `ptg_${ptg}`;
         menuPlacesToGo.appendChild(currentPTG);
     }))
 
+    // ADD EVENT LISTENERS!
 
+    // person talk
+    for(i = 0; i < CURRENT_STATE.currentMap.mapMenu.personsAtPlace.length; i++){
+        let person = CURRENT_STATE.currentMap.mapMenu.personsAtPlace[i];
+        document.getElementById(`talk_${person.personName}`).addEventListener("click", function(){
+            typeof(person.personTalk[person.personTalkedTo]) == "string" ? writeText(person.personTalk[person.personTalkedTo], `BESZÉLEK VELE: ${person.personName}`) : person.personTalk[person.personTalkedTo]();
+            if(person.personTalkedTo < person.personTalk.length-1){person.personTalkedTo++}
+        }, false);
+    }
 
+    // person romance
+    for(i = 0; i < CURRENT_STATE.currentMap.mapMenu.personsAtPlace.length; i++){
+        let person = CURRENT_STATE.currentMap.mapMenu.personsAtPlace[i];
+        document.getElementById(`romance_${person.personName}`).addEventListener("click", function(){
+            typeof(person.personRomance[person.personRomanced]) == "string" ? writeText(person.personRomance[person.personRomanced], `KIKEZDEK VELE: ${person.personName}`) : person.personRomance[person.personRomanced]();
+            if(person.personRomanced < person.personRomance.length-1){person.personRomanced++}
+        }, false);
+    }
 
+    // points of interest
+    for(i = 0; i < CURRENT_STATE.currentMap.mapMenu.pointsOfInterest.length; i++){
+        let poi = CURRENT_STATE.currentMap.mapMenu.pointsOfInterest[i];
+        document.getElementById(`poi_${poi.poiName}`).addEventListener("click", function(){
+            typeof(poi.poiEvent[poi.poiDone]) == "string" ? writeText(poi.poiEvent[poi.poiDone], poi.poiName) : poi.poiEvent[poi.poiDone]();
+            if(poi.poiDone < poi.poiEvent.length-1){poi.poiDone++}
+        }, false);
+    }
+
+    // places to go
+    
+
+    window.scrollTo(0, 0);
 }
 
 
 
+// CURRENT STATE OF CHARACTERS, MAPS AND STUFF
+
+var CURRENT_STATE = {
+    currentMap: maps[0],
+    party: [characters[0]],
+    inventory: [items[0], items[1]],
+}
+
 // INITIALIZE GAME
 loadParty();
 loadInventory();
-loadMap(maps[0  ]);
+loadMap(maps[0]);
+
+addToInventory("test item 2");
+writeText("test text body", "test text title");
